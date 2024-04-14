c189ab01bf02545f593988e8927716d5372fac5d
nwitat <146670170+monir65489846@users.noreply.github.com>
2024-04-14 15:26:05 +0100

    Update 21.js

--- 21.js
+++ 21.js
@@ -32,11 +32,4 @@
       downloadLink02.style.display = 'block';
     }
 
-    function downloadFile02() {
-      const combinedContent02 = document.getElementById('combinedContent02').value;
-      const blob = new Blob([combinedContent02], { type: 'text/plain' });
-      const url = URL.createObjectURL(blob);
-
-      const downloadLink02 = document.getElementById('downloadLink02');
-      downloadLink02.href = url;
-    }
+   
