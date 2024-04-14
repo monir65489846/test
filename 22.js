08ef955b507ca076a9cd2cd12fe8d88d5ceb8d60
nwitat <146670170+monir65489846@users.noreply.github.com>
2024-04-14 15:27:06 +0100

    Add files via upload

---
+++ 22.js
@@ -0,0 +1,8 @@
+﻿ function downloadFile02() {
+      const combinedContent02 = document.getElementById('combinedContent02').value;
+      const blob = new Blob([combinedContent02], { type: 'text/plain' });
+      const url = URL.createObjectURL(blob);
+
+      const downloadLink02 = document.getElementById('downloadLink02');
+      downloadLink02.href = url;
+    }
